6fe08c58eb8115d577968c2181a3109430732651
faldymartinus <faldymartinus@gmail.com>
2023-06-02 15:51:17 +0700

    add create var file

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -32,6 +32,33 @@ const kafkaSave = (req, res) => {
 
 const hadoopSave = (req, res) => {
     console.log(req.body.hadoopIp)
+    const fs = require('fs'); 
+    var jsonData = `{
+        "vm": {
+            "hadoop": {
+                "ip": ${req.body.hadoopIp}
+            }
+        }
+    }`
+
+
+ 
+    // parse json
+    var jsonObj = JSON.parse(jsonData);
+    console.log(jsonObj);
+    
+    // stringify JSON Object
+    var jsonContent = JSON.stringify(jsonObj);
+    console.log(jsonContent);
+    
+    fs.writeFile("outputs.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+    
+        console.log("JSON file has been saved.");
+    });
 }
 
 module.exports =  {
