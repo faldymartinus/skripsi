31e55d29f5912463fc90f52cbfe0e0f1dabdb4c5
faldymartinus <faldymartinus@gmail.com>
2023-06-03 15:40:27 +0700

    fix wrtie files

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -33,22 +33,35 @@ const kafkaSave = (req, res) => {
 const hadoopSave = (req, res) => {
     
     const fs = require('fs'); 
-    var data = fs.readFileSync('outputs.json');
+    try {
+        var data = fs.readFileSync('variables.json');
+    } catch (error) {
+        console.log("done")
+        data = {}
+        writeToFiles(data);
+
+    }
+    
     var myObject= JSON.parse(data);
     var newkey = {key3: { key4 : req.body.hadoopIp}}
    
-    Object.assign(myObject.vm2, newkey);
+    //append key to object
+    Object.assign(myObject, newkey);
 
+    //write variable to file
     console.log(myObject)
 
-    var newData2 = JSON.stringify(myObject);
-    fs.writeFile("data2.json", newData2, (err) => {
-        
-    // Error checking
-    if (err) throw err;
-    console.log("New data added");
-    });
-    }
+    writeToFiles(myObject);
+}
+
+function writeToFiles(data){
+    const fs = require('fs'); 
+    var newData2 = JSON.stringify(data);
+    fs.writeFile("variables.json", newData2, (err) => {
+        if (err) throw err;
+        console.log("New data added");
+        });
+}
 
 module.exports =  {
     mainView,
